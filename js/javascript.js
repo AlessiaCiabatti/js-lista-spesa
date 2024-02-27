a18cf1849324b6be6e2d8fb25257e75824726a7f
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-02-27 14:47:29 +0100

    [add] js-while

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,25 @@
+// 1 
+// arrey lista spesa
+let listaSpesa = [
+  'latte',
+  'pane',
+  'pasta',
+  'sugo',
+  'carne',
+  'pesce',
+  'verdure',
+]
 
+// 2
+// contatore = 0 che si incrementerà nel ciclo
+i = 0;
+
+// 3
+// ciclo while i < lista
+while (i < listaSpesa.length){
+  // 4
+  console.log(listaSpesa[i]);
+  // 5
+  // incremento di i fino a quando non raggiunge la lunghezza della lista
+  i++;
+}
